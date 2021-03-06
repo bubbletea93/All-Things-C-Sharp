0013814d98a1ac03967b2f172f92f7acddb59ab3
bubbletea93 <dcnguyen93@yahoo.com>
2021-03-06 02:06:32 -0500

    Add ClientRepository Interface

--- Refactored/LegacyApp/Repositories/ClientRepository.cs
+++ Refactored/LegacyApp/Repositories/ClientRepository.cs
@@ -6,7 +6,7 @@ using System.Data.SqlClient;
 
 namespace LegacyApp.Repositories
 {
-    public class ClientRepository
+    public class ClientRepository : IClientRepository
     {
         public Client GetById(int id)
         {
---
+++ Refactored/LegacyApp/Repositories/IClientRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace LegacyApp.Repositories
+{
+    using LegacyApp.Models;
+
+    public interface IClientRepository
+    {
+        public Client GetById(int id);
+    }
+}
