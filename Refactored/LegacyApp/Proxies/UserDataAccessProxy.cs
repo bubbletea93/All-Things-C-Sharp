07a507a4a664e1b2cb2a289c1f5b7db0a65ad1aa
bubbletea93 <dcnguyen93@yahoo.com>
2021-03-06 02:06:00 -0500

    Add UserDataAccessProxy to call a static UserDataAccess

---
+++ Refactored/LegacyApp/Proxies/UserDataAccessProxy.cs
@@ -0,0 +1,13 @@
+﻿namespace LegacyApp.Proxies
+{
+    using LegacyApp.DataAccess;
+    using LegacyApp.Models;
+
+    class UserDataAccessProxy : IUserDataAccess
+    {
+        public void AddUser(User user)
+        {
+            UserDataAccess.AddUser(user);
+        }
+    }
+}
